b51538336333ea0922d5fbde9e76c93df76cfde9
KaueSanchesG <kauesanches123@gmail.com>
2026-06-28 11:05:47 -0300

    first commit

---
+++ src/test/java/notify/firebase/kauesanchesg/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package notify.firebase.kauesanchesg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

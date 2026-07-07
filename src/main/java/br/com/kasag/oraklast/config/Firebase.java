374d55a82508d1291b07d107c05a95cb06d6d05e
KaueSanchesG <kauesanches123@gmail.com>
2026-07-06 23:58:11 -0300

    feat: initializing rtdb condiguration

--- src/main/java/br/com/kasag/oraklast/config/Firebase.java
+++ src/main/java/br/com/kasag/oraklast/config/Firebase.java
@@ -3,6 +3,7 @@ package br.com.kasag.oraklast.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.*;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -27,6 +28,21 @@ public class Firebase {
                         .build();
 
                 FirebaseApp.initializeApp(op);
+
+                DatabaseReference rf = FirebaseDatabase.getInstance()
+                        .getReference("restricted_access/secret_doc");
+                rf.addListenerForSingleValueEvent(new ValueEventListener(){
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot){
+                        Object doc = dataSnapshot.getValue();
+                        System.out.println(doc);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error){
+
+                    }
+                });
             }
         } catch (IOException e) {
             throw new RuntimeException("Erro ao inicializar a conexão com o firebase", e);
